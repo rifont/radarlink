c33edcef52fd2ed2123f3f4fce54e299dd3afc03
Richard Fontein <richard@fontein.co>
2023-09-15 18:40:08 +0100

    feat: Add shadcn component base

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,16 @@
 import ms from "ms";
 
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
   if (!timestamp) return "never";
-  return `${ms(Date.now() - new Date(timestamp).getTime())}${
-    timeOnly ? "" : " ago"
-  }`;
+  return `${ms(Date.now() - new Date(timestamp).getTime())}${timeOnly ? "" : " ago"
+    }`;
 };
 
 export async function fetcher<JSON = any>(
