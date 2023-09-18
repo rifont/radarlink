611ade8f939118dbe325eb094211c1e0b9acb857
Richard Fontein <32132657+richardfontein@users.noreply.github.com>
2023-09-18 15:01:36 +0100

    style: Use moonstart icon for dark mode

--- components/shared/mode-toggle.tsx
+++ components/shared/mode-toggle.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun, Monitor } from "lucide-react"
+import { MoonStar, Sun, Monitor } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
@@ -20,7 +20,7 @@ export function ModeToggle() {
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <MoonStar className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
@@ -29,7 +29,7 @@ export function ModeToggle() {
                     <Sun className="mr-2 h-[1.2rem] w-[1.2rem]" /> Light
                 </DropdownMenuItem>
                 <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    <Moon className="mr-2 h-[1.2rem] w-[1.2rem]" /> Dark
+                    <MoonStar className="mr-2 h-[1.2rem] w-[1.2rem]" /> Dark
                 </DropdownMenuItem>
                 <DropdownMenuItem onClick={() => setTheme("system")}>
                     <Monitor className="mr-2 h-[1.2rem] w-[1.2rem]" /> System
